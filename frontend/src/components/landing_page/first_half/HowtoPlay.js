a53dfa74dcb2130af16b1344d8b1a1ca58331ca1
FinnMcTavish <71775895+FinnMcTavish@users.noreply.github.com>
2022-08-22 11:53:31 +0530

    Added HowtoPlay component

---
+++ frontend/src/components/landing_page/first_half/HowtoPlay.js
@@ -0,0 +1,39 @@
+import React from "react";
+import  "./../../../assets/howtoplaystyle.css";
+
+class HowtoPlay extends React.Component
+{
+    render()
+    {
+        return(
+            <div className="bodys">
+                <center><h1 className="howtoplay">How to Play</h1></center>
+
+                <div className="cards-list">
+  
+                    <div className="card">
+                    <div className="card_title title-white">
+                        <p>How to play ?</p>
+                    </div>
+                    </div>
+            
+                    <div className="card ">
+                    <div className="card_title title-white">
+                        <p>How to play ?</p>
+                    </div>
+                    </div>
+
+                    <div className="card ">
+                        <div className="card_title title-white">
+                        <p>How to play ?</p>
+                        </div>
+                    </div>
+
+            
+                </div>
+            </div>
+
+            
+        );
+    }
+}
