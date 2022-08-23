c90cf8ab6d60281dab4aaf4cac6a94416ee0348a
Rithvik008 <rithvikhariprasad@gmail.com>
2022-08-23 20:40:27 +0530

    Bug fix

--- frontend/src/components/profilePage/ProfileCard/ProfileCard.js
+++ frontend/src/components/profilePage/ProfileCard/ProfileCard.js
@@ -6,43 +6,47 @@ import point from "../../../assets/point.png";
 function ProfileCard() {
   return (
     <section className="profcard">
-      <div className="rect">
-        <div className="circle">
-          <img src={pic}></img>
-        </div>
-        <div className="circle">
-          <div className="parent">
-            <center>
-              <h2>Profile Name</h2>
-            </center>
-            <div className="points-container">
-              <h3 className="points">POINTS</h3>
-              <h3 className="points-num">15</h3>
-              <img classname="coin" src={point}></img>
+      <center>
+        <div className="rect">
+          <div className="circle">
+            <img src={pic}></img>
+          </div>
+          <div className="circle">
+            <div className="parent">
+              <center>
+                <h2>Profile Name</h2>
+              </center>
+              <div className="points-container">
+                <h3 className="points">POINTS</h3>
+                <h3 className="points-num">15</h3>
+                <img classname="coin" src={point}></img>
+              </div>
+
+              <h4 className="play-from">
+                Started Playing from{" "}
+                <span className="date">16th August 2022</span>
+              </h4>
             </div>
+          </div>
 
-            <h4 className="play-from">
-              Started Playing from{" "}
-              <span className="date">16th August 2022</span>
-            </h4>
+          <div className="square-parent">
+            <div className="square-container">
+              <Square title="IBM" />
+              <Square title="XYZ" />
+              <Square title="AAA" />
+              <Square title="BBB" />
+            </div>
+            <div className="square-container">
+              <Square title="CCC" />
+              <Square title="DDD" />
+              <Square title="EEE" />
+              <Square title="FFF" />
+            </div>
           </div>
         </div>
 
-        <div className="square-parent">
-          <div className="square-container">
-            <Square title="IBM" />
-            <Square title="XYZ" />
-            <Square title="AAA" />
-            <Square title="BBB" />
-          </div>
-          <div className="square-container">
-            <Square title="CCC" />
-            <Square title="DDD" />
-            <Square title="EEE" />
-            <Square title="FFF" />
-          </div>
-        </div>
-      </div>
+      </center>
+      
     </section>
   );
 }
