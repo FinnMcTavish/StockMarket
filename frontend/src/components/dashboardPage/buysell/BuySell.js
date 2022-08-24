c0c5f4a3340608210bab8713eb7247cfe4013478
FinnMcTavish <71775895+FinnMcTavish@users.noreply.github.com>
2022-08-24 14:15:42 +0530

    Add files via upload

--- frontend/src/components/dashboardPage/buysell/BuySell.js
+++ frontend/src/components/dashboardPage/buysell/BuySell.js
@@ -7,6 +7,9 @@ class BuySell extends Component {
   render() {
     return (
       <div className="container">
+        <div className="Borders">
+        <div className="innerBox">
+
         <div className="com">IBM</div>
         <div className="fitter">
           <GraphMaker product="IBM" height={300} width={550} />
@@ -15,7 +18,79 @@ class BuySell extends Component {
           <button className="green">Buy</button>
           <button className="red">Sell</button>
         </div>
+        </div>
+        <div className="innerBox">
+
+        <div className="com">TSCO.LON</div>
+        <div className="fitter">
+          <GraphMaker product="TSCO.LON" height={300} width={550} />
+        </div>
+        <div className="colcontainer">
+          <button className="green">Buy</button>
+          <button className="red">Sell</button>
+        </div>
+
+        </div>
+
+        <div className="innerBox">
+
+        <div className="com">DAI.DEX</div>
+        <div className="fitter">
+          <GraphMaker product="DAI.DEX" height={300} width={550} />
+        </div>
+        <div className="colcontainer">
+          <button className="green">Buy</button>
+          <button className="red">Sell</button>
+        </div>
+
+        </div>
+
+        <div className="innerBox">
+
+        <div className="com">SHOP.TRT</div>
+        <div className="fitter">
+          <GraphMaker product="SHOP.TRT" height={300} width={550} />
+        </div>
+        <div className="colcontainer">
+          <button className="green">Buy</button>
+          <button className="red">Sell</button>
+        </div>
+
+        </div>
+
+        <div className="innerBox">
+
+        <div className="com">GPV.TRV</div>
+        <div className="fitter">
+          <GraphMaker product="GPV.TRV" height={300} width={550} />
+        </div>
+        <div className="colcontainer">
+          <button className="green">Buy</button>
+          <button className="red">Sell</button>
+        </div>
+
+        </div>
+
+        <div className="innerBox">
+
+        <div className="com">RELIANCE.BSE</div>
+        <div className="fitter">
+          <GraphMaker product="RELIANCE.BSE" height={300} width={550} />
+        </div>
+        <div className="colcontainer">
+          <button className="green">Buy</button>
+          <button className="red">Sell</button>
+        </div>
+
+        </div>
+
+
+
+
       </div>
+      </div>
+
+      
     );
   }
 }
