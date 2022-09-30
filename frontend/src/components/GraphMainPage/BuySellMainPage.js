23a4ae45216afddf78f9d88156f9e3634bc6cfd9
daniel-jebarson <danieljebarson21@gmail.com>
2022-10-01 01:09:15 +0530

    added buyItem and other functions

--- frontend/src/components/GraphMainPage/BuySellMainPage.js
+++ frontend/src/components/GraphMainPage/BuySellMainPage.js
@@ -15,35 +15,48 @@ import GraphMaker from "./GraphMakerMainPage";
 class BuySellMainPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { product: "IBM", userData: {} };
+    this.state = {
+      product: "IBM",
+      number: 0,
+      userData: {},
+      todayPrice: 0,
+      productPrice: 0,
+      buyGain: 0,
+      sellGain: 0,
+    };
   }
 
   componentDidMount() {
+    this.setUserProductData();
+    this.setTodayPrice(this.state.product);
+    this.setProfit();
+  }
+
+  setProfit = () => {
+    this.setState({
+      buyGain: this.state.todayPrice - this.state.productPrice,
+      sellGain: this.state.productPrice - this.state.todayPrice,
+    });
+  };
+
+  setUserProductData = async () => {
     const username = sessionStorage.getItem("username");
 
-    Axios.get("http://localhost:3002/getData").then((response) => {
+    await Axios.get("http://localhost:3002/getData").then((response) => {
       for (var i = 0; i < response.data.length; i++) {
         if (response.data[i]["username"] === username) {
-          this.setState({ userData: response.data[i] });
+          this.setState({
+            userData: response.data[i],
+            productPrice: response.data[i][this.state.product]["cp"],
+          });
+          // console.log(this.state.userData);
         }
       }
     });
-  }
+  };
 
-  // getYesterday() {
-  //   var d = new Date(new Date().valueOf() - 1000 * 60 * 60 * 24),
-  //     month = "" + (d.getMonth() + 1),
-  //     day = "" + d.getDate(),
-  //     year = d.getFullYear();
-
-  //   if (month.length < 2) month = "0" + month;
-  //   if (day.length < 2) day = "0" + day;
-
-  //   return [year, month, day].join("-");
-  // }
-
-  buyItem = async () => {
-    const API_REQ = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=demo`;
+  setTodayPrice = async (product) => {
+    const API_REQ = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${product}&outputsize=full&apikey=demo`;
     await fetch(API_REQ)
       .then((res) => {
         return res.json();
@@ -53,10 +66,50 @@ class BuySellMainPage extends Component {
           data["Time Series (Daily)"][
             Object.keys(data["Time Series (Daily)"])[0]
           ][["1. open"]];
-
-        console.log(lastValue);
+        this.setState({ todayPrice: lastValue });
       });
+  };
 
+  buyItem = async () => {
+    if (this.state.number <= 0) {
+      alert("Please specify the proper stock value");
+      this.nameInput.focus();
+      return;
+    } else if (this.state.number == undefined || this.state.number == "") {
+      alert("Please specify the number of tasks");
+      this.nameInput.focus();
+      return;
+    }
+    if (this.state.userData.coins < this.state.number * this.state.todayPrice) {
+      alert(
+        `Sorry you don't have enough money to buy ${this.state.number} ${
+          this.state.product
+        } which costs ${
+          this.state.number * this.state.todayPrice
+        } coins. Number of coins you have: ${this.state.userData.coins} `
+      );
+      return;
+    } else {
+      let stocks = this.state.userData[this.state.product]["stocks"];
+      let times = this.state.userData[this.state.product]["times"];
+      let cp = this.state.productPrice;
+      let buyPrice = this.state.number * this.state.todayPrice;
+      const coins = this.state.userData.coins - buyPrice;
+      cp = (cp * times + buyPrice) / (times + 1);
+      times += 1;
+      stocks += this.state.number;
+      alert("Stocks bought successfully!");
+      console.log(
+        `${this.state.number} ${this.state.product} stocks bought for ${buyPrice}. Coins have ${coins}. Avg = ${cp}, times= ${times}. Number of ${this.state.product} stocks= ${stocks}`
+      );
+    }
+    //  product: "IBM",
+    //   number: 0,
+    //   userData: {},
+    //   todayPrice: 0,
+    //   productPrice: 0,
+    //   buyGain: 0,
+    //   sellGain: 0,
     console.log("Bought item");
   };
   sellItem() {
@@ -124,7 +177,18 @@ class BuySellMainPage extends Component {
             </ul>
           </nav>
         </div>
-        <input type={"number"} required defaultValue={"0"} />
+        <input
+          placeholder="Enter number of stocks"
+          type={"number"}
+          required
+          min={0}
+          ref={(input) => {
+            this.nameInput = input;
+          }}
+          onChange={(e) => {
+            this.setState({ number: e.target.value });
+          }}
+        />
         <div className="colcontainer">
           <button onClick={this.buyItem} className="green">
             Buy
