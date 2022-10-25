a5541e49cab49182e39179e193dd9279d0dff503
daniel-jebarson <danieljebarson21@gmail.com>
2022-10-25 12:52:33 +0530

    bug fix ✔️

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ const App = () => {
 
         <Route
           path="/profile"
-          element={!username ? <Navigate to="/login" /> : <Profile />}
+          element={!username ? <Navigate to="/registration" /> : <Profile />}
         />
       </Routes>
     </BrowserRouter>
